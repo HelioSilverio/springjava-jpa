bb88e9d646d02d4b6060aebb6140beb721ded1fc
Helio Silverio <silveriosil@yahoo.com.br>
2023-08-10 21:23:51 -0300

    Exception handling - update

--- src/main/java/com/silverio/Curso/services/UserService.java
+++ src/main/java/com/silverio/Curso/services/UserService.java
@@ -13,6 +13,8 @@ import com.silverio.Curso.repositories.UserRepository;
 import com.silverio.Curso.resources.execeptions.DatabaseException;
 import com.silverio.Curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,10 +45,17 @@ public class UserService {
 	}
 	
 	public User update(Long id,User obj) {
+		try {
 		User entity=repository.getReferenceById(id);
 		updateData(entity,obj);		
-		return repository.save(entity);
+	return repository.save(entity);
+	} catch (EntityNotFoundException e) {
+		throw new ResourceNotFoundException (id) ;
+	 }
 	}
+			
+	
+	
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
